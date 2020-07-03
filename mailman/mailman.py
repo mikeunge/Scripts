5dc94120b4090fc5288612aa34df98006f3af9d7
mikeunge <mikeunge@protonmail.com>
2020-07-03 10:28:46 +0200

    💬 Workflow redone; added configuration; implemented classes; etc..

--- mailman/mailman.py
+++ mailman/mailman.py
@@ -2,69 +2,94 @@
 # -*- coding: utf-8 -*-
 #
 # mailman.py
-# version: 1.0.0
+# version: 1.0.2
+# build: alpha
 #
 # Author:	Ungerböck Michele
 # Github:	github.com/mikeunge
 # Company:	GEDV GmbH
 #
 # All rights reserved.
+#
 import smtplib
 import ssl
-from credentials import credentials as cred
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+# Get the configuration.
+from config import mail, config
 
 
-def main():
-    # Create the message and the email headers.
-    message = MIMEMultipart("alternative")
-    message["Subject"] = "IP address has changed!"
-    message["From"] = cred["sender_name"]
-    message["To"] = cred["receiver_email"]
+class Mail:
+    def __init__(self):
+        """
+            Initialize and define the config items.
+        """
+        self.smtp_server = config['server']['smtp_server']
+        self.smtp_port = int(config['server']['smtp_port'])     # MUST be an integer.
+        self.send_mail = mail['sender']['email']
+        self.send_name = mail['sender']['name']
+        self.send_pass = mail['sender']['password']
+        self.rec_mail = mail['receiver']['email']
+        self.rec_name = mail['receiver']['name']
+        self.subject = config['mail']['subject']
+        self.prefix = config['mail']['prefix']
 
-    # Create the mail content.
-    text = f"""\
-        Status,\n
-        sender: {cred["sender_email"]}\n
-        status: {cred["sender_name"]}
-    """
-    html = f"""\
-    <html>
-        <body>
-            <p>Status,<br>
-            sender: {cred["sender_email"]}<br>
-            sender: {cred["sender_name"]}<br>
-            </p>
-        </body>
-    </html>
-    """
 
-    # Define the MimeTexts (plain/html)
-    plain_text = MIMEText(text, "plain")
-    html_text = MIMEText(html, "html")
+    def create_message(self):
+        """
+            Get all the information the message needs and construct it.
+            Returns the actual message.
+            ! Plain-Text only !
+        """
+        # Create the message and the email headers.
+        message = MIMEMultipart("alternative")
+        message["Subject"] = f"{self.prefix} - {self.subject}"
+        message["From"] = self.send_name
+        message["To"] = self.rec_mail
 
-    # Attach the text's to the email message.
-    message.attach(plain_text)
-    message.attach(html_text)
+        # Create the mail content.
+        text = f"""\
+            Status,\n
+            sender: {self.send_name}\n
+            status: {self.send_mail}
+        """
+        # Define the MimeTexts (plain/html)
+        plain_text = MIMEText(text, "plain")
+        # Attach the text's to the email message.
+        message.attach(plain_text)
+        return message
 
-    # Create an ssl connection.
-    context = ssl.create_default_context()
-    with smtplib.SMTP(cred["mail_server"], cred["port"]) as conn:
-        try:
-            conn.ehlo()  # Can be omitted
-            conn.starttls(context=context)
-            conn.ehlo()  # Can be omitted
-            
-            # Connect to the mail server.
-            conn.login(cred["sender_email"], cred["password"])
-            # Send the email.
-            conn.sendmail(cred["sender_email"], cred["receiver_email"], message.as_string())
-        except Exception as ex:
-            print(f"Something went wrong while sending the email.. Error: {ex}")
-        finally:
-            print("Ende")
+
+    def send(self):
+        """
+            This function connects to the server and sends the e-mail.
+        """
+        message = self.create_message()
+        # Create an ssl connection.
+        context = ssl.create_default_context()
+        # TODO: New/Better error handling..
+        with smtplib.SMTP(self.smtp_server, self.smtp_port) as conn:
+            try:
+                conn.ehlo()  # Can be omitted
+                conn.starttls(context=context)
+                conn.ehlo()  # Can be omitted
+                
+                # Connect to the mail server.
+                conn.login(self.send_mail, self.send_pass)
+                # Send the email.
+                conn.sendmail(self.send_mail, self.rec_mail, message.as_string())
+            except Exception as ex:
+                print(f"Something went wrong while sending the email.. Error: {ex}")
+            finally:
+                print("Ende")
 
 
 if __name__ == '__main__':
-    main()
+    # TODO: implement the logging handler..
+    # Get logging information, create the logger.
+    #log_file = config['logging']['file']
+    #log_level = config['logging']['level']
+    mail = Mail()
+    mail.send()
+    # TODO: Exit with errors, redo the actual handling and exiting proccess.
+    exit()
