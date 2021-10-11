bc3e5f9ff699742ac67ae1e43fbe4f3a34e28bc6
Mike Unge <mikeunge@protonmail.com>
2021-10-11 15:14:58 +0200

    add: loading config files works and errors are handled, added debug print - just run ./wpe.py debug

--- WallpaperEngine/wpe.py
+++ WallpaperEngine/wpe.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+import sys
 import json
 from os import walk, path, system
 from random import randint
+
+
+# dbg :: print debug messages
+def dbg(msg):
+    if DEBUG:
+        print(f'[debug] {msg}')
 
 
 # get_wallpaper :: takes the path where the wallpapers are stored and returns an array with all the available items.
@@ -17,13 +24,21 @@ def get_wallpapers(wp_path):
 
 # set_wallpaper :: sets the wallpaper, as param it needs the FULL path to the wallpaper.
 def set_wallpaper(wallpaper):
+    # if wallpaper has whitespaces, make it linux (feh) friendly
+    wallpaper = wallpaper.replace(" ", "\\ ")
+    dbg(f'setting wallpaper: {wallpaper}')
     system(f'feh --bg-scale {wallpaper}')
 
 
 # load_config :: load a configuration file.
 def load_config(file):
-    with open(file, 'r') as f:
-        config = json.load(f)
+    dbg(f'loading config: {file}')
+    try:
+        with open(file, 'r') as f:
+            config = json.load(f)
+    except Exception as e:
+        print(f'Could not load config file ({file}).\nError: {e}')
+        config = {}
     return config 
 
 
@@ -43,9 +58,15 @@ def main(conf):
         set_wallpaper(path.join(conf['wp_path'], conf['wp']))
     else:
         # if we cannot find the image, we set random to true and run the script again
+        dbg(f'wallpaper ({conf["wp"]}) not found, selecting random image')
         conf['random'] = True
         main(conf)
 
+
+DEBUG = False
+if len(sys.argv) > 1:
+    if sys.argv[1] == 'debug':
+        DEBUG = True
 
 if __name__ == '__main__':
     config_paths = ['wpe.json', '~/.config/wpe.json', '~/.config/wpe/wpe.json']
@@ -59,7 +80,8 @@ if __name__ == '__main__':
     if config == {}:
         config = {
             'wp_path': '~/Pictures/Wallpaper/',  # make sure to add the trailing slash
-            'wp': 'berserk.png',   # if you want a fixed wp, enter the name here, not the path!
-            'random': False,    # set this to 'False' if you want to used a fixed wp
+            'wp': '',   # if you want a fixed wp, enter the name here
+            'random': True,    # set this to 'False' if you want to used a fixed wp
         }
+    dbg(f'config dump: {config}')
     main(config)
